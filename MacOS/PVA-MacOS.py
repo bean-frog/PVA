e45bb2eda98dfc05dbf29883db5cc98421eae7ef
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-12-14 15:01:25 -0800

    Create PVA-MacOS.py

---
+++ MacOS/PVA-MacOS.py
@@ -0,0 +1,6 @@
+import time
+import subprocess
+
+while True:
+    subprocess.run(["osascript", "-e", 'display notification "BITCH YOU HAD BETTER BE STUDYING OR SO HELP ME GOD I WILL BEAT THE EVERLOVING SHIT OUT OF YOU. THERE IS NO WAY ON EARTH YOU GOT FUCKING DISTRACTED AGAIN YOU USELESS PIECE OF HUMAN GARBAGE" with title "GET THE FUCK BACK ON TASK"']
+    time.sleep(600)
