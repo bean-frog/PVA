c4c95bcb69d4146b03a17314037dd504bc626479
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-12-14 14:41:06 -0800

    Update setup.zsh

--- MacOS/setup.zsh
+++ MacOS/setup.zsh
@@ -1,1 +1,21 @@
 
+if ! [ -x "$(command -v pip)" ]; then
+  echo "Installing pip..."
+  sudo apt-get install python-pip
+fi
+
+echo "Installing required module: playsound"
+pip install playsound
+read -p "Launch PVA now? " response
+
+# Check if the response is yes
+if [ "$response" == "yes" ]; then
+  # Run the macos.py script if the response is yes
+  python PVA-MacOS.py
+fi
+
+
+
+
+
+
