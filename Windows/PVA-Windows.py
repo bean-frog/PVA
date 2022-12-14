227074c9447bd530f39fb50694b96082ed23c237
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-12-14 15:29:22 -0800

    Update PVA-Windows.py

--- Windows/PVA-Windows.py
+++ Windows/PVA-Windows.py
@@ -3,7 +3,21 @@ import win10toast
 
 toaster = win10toast.ToastNotifier()
 
-while True:
+option = input("Choose harshness level: (low, mid, or high): ")
+
+if option == 'high':
+  while True:
     toaster.show_toast("BITCH YOU HAD BETTER BE STUDYING OR SO HELP ME GOD I WILL BEAT THE EVERLOVING SHIT OUT OF YOU. THERE IS NO WAY ON EARTH YOU GOT FUCKING DISTRACTED AGAIN YOU USELESS PIECE OF HUMAN GARBAGE","GET THE FUCK BACK ON TASK",
                        duration=10)
     time.sleep(600)
+    
+elif option == 'mid':
+    
+    
+elif option == 'low':
+  
+
+else:
+  print("Invalid option, try again")
+
+
