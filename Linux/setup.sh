21ba96f175223773fc9476d3ff111a4cfe03e1e4
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-12-14 14:51:55 -0800

    Update setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,1 +1,11 @@
+if ! [ -x "$(command -v pip)" ]; then
+  echo "Your don't have pip installed, installing it for you..."
+  sudo apt-get install python-pip
+fi
+echo "Installing required module: playsound"
+pip install playsound
+read -p "Run PVA now? [type yes/no] " response
 
+if [ "$response" == "yes" ]; then
+  python PVA-Linux.py
+fi
