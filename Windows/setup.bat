5393e2f9985588d65ef1c0ffa941db3bc8f51632
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-12-14 15:08:12 -0800

    Update setup.bat

--- Windows/setup.bat
+++ Windows/setup.bat
@@ -1,15 +1,15 @@
 pip --version
 IF %ERRORLEVEL% NEQ 0 (
   echo You don't have pip installed, installing it for you...
-  python -m ensurepip --default-pip >NUL
+  python -m ensurepip --default-pip 
 )
 
 echo Installing required module: playsound.
-pip install playsound >NUL
+pip install playsound 
 echo Installing required module: win10toast
-pip install win10toast >NUL
+pip install win10toast 
 
 set /p question="Launch PVA now? (type y/n) "
 if /i "%question%"=="y" (
-  python PVA-Windows.py >NUL
+  python PVA-Windows.py 
 )
