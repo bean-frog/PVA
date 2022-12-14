71dee235faed0889c24813205219478f6379fb42
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-12-14 15:02:21 -0800

    Delete PVA-Windows.py

--- PVA-Windows.py
+++
@@ -1,9 +0,0 @@
-import time
-import win10toast
-
-toaster = win10toast.ToastNotifier()
-
-while True:
-    toaster.show_toast("BITCH YOU HAD BETTER BE STUDYING OR SO HELP ME GOD I WILL BEAT THE EVERLOVING SHIT OUT OF YOU. THERE IS NO WAY ON EARTH YOU GOT FUCKING DISTRACTED AGAIN YOU USELESS PIECE OF HUMAN GARBAGE","GET THE FUCK BACK ON TASK",
-                       duration=10)
-    time.sleep(600)
